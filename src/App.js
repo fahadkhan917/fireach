4a8255990cdc8ead63e44cd377ce02ced8c894dc
fahadkhan917 <fk16181@gmail.com>
2020-06-17 19:28:24 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div><h1>hello world</h1>
+    <div><h1>hello world fahad</h1>
 </div>      
   );
 }
